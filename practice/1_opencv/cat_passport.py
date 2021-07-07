95829f9991fae794bd16b1c407f24b05fee7e8ad
ertimofeev <58752172+ertimofeev@users.noreply.github.com>
2021-07-07 19:34:40 +0300

    Add files via upload

--- practice/1_opencv/cat_passport.py
+++ practice/1_opencv/cat_passport.py
@@ -4,31 +4,46 @@ import cv2 as cv
 
 #cd C:\Users\Admin\Desktop\summer\CV-SUMMER-CAMP-2021\practice\1_opencv
 #cat_passport -i cat.jpg -m haarcascade_frontalcatface.xml
+
 def make_cat_passport_image(input_image_path, haar_model_path):
 
     # Read image
     img = cv.imread(input_image_path)
-    #print(img)
+    
     # Convert image to grayscale
-
+    gray = cv.ctvColor(img, cv.COLOR_BGR2GRAY)
+    
     # Normalize image intensity
-
+    norm_gray = cv.equalizeHist(gray)
+    
     # Resize image
+    small = cv.resize(img, (100, 75), interpolation = INTER_CUBIC)
 
     # Detect cat faces using Haar Cascade
     detector = cv.CascadeClassifier(haar_model_path)
     rects = detector.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5, minSize=(75,75))
     print(rects)
+                      
     # Draw bounding box
+    for (i, (x,y,w,h))in enumerate(rects)
+        cv.rectangle(img, (x,y), (x+w, y+w), (0,0,255),2)
+        cv.putText(img, "Cat #".format(i+1), (x,y-10), cv.FRONT_HERSHEY_SIMPLEX, 0.55, (0,0,255), 2)
+        
 
     # Display result image
     cv.imshow("cat", img)
     cv.waitKey(0)
-    cv.ddestroyAllWindows();
+    cv.destroyAllWindows();
+    
     # Crop image
-
+    x, y, w, h=rects[0]
+    face = img[y:y+h,x:x+w]
+        
     # Save result image to file
-
+    for (i, (x,y,w,h))in enumerate(rects)
+        cv.rectangle(img, (x,y), (x+w, y+w), (0,0,255),2)
+        cv.imwrite(img, "Cats_fase".format(i+1)+".jpg", img[y:y+h,x:x+w])
+    
     return
 
 
